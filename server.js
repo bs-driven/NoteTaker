74c7cad13d65c493098b5c6b7dab1f22ff6bcbc7
Bradley <bradsamsess90@icloud.com>
2022-12-16 22:35:38 -0500

    heroku attempt

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const api =require('./routes/index.js');
 
-const PORT = process.env.port || 3007;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
