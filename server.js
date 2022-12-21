b904ed6c952d6cc912034dac7223ab1a0fc01d6e
Bradley <bradsamsess90@icloud.com>
2022-12-20 19:00:33 -0500

    quick push

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const api =require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3007;
 
 const app = express();
 
